998b3f4a01d5c32d62915a05ed2210bd608250a0
Ibtasham Ali <aleejaan562@gmail.com>
2022-05-28 01:49:15 +0500

    axios and adds api service
    Signed-off-by: Ibtasham Ali <ibtasham.ali@emerssive.com>

---
+++ src/generic-services/api.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+// import auth from "../auth";
+
+axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('access_token')}`;
+
+class Api {
+  execute(url,method, dataObject={}) {
+    return new Promise(function (resolve, reject) {
+      const authAxios = axios.create({
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        },
+      })
+      authAxios({
+        method: method,
+        url: url,
+        data: dataObject,
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then((res) =>{
+        resolve(res);
+      }).catch((err) =>{
+        if (err.response.status === 401) {
+          // auth.logout();
+        }
+        reject(err);
+      })
+    })
+  }
+
+  post(url, data = {}) {
+    return new Promise((resolve, reject )=> {
+      axios({
+        url: url,
+        data: data,
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then(res => {
+        resolve(res);
+        console.log(res);
+      }).catch(err => {
+        reject(err);
+      });
+    })
+  }
+}
+
+export default new Api();
